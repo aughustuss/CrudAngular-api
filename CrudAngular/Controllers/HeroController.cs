4b76bbb0a189251b009d52986f8b0019bbb1a602
augusto <augusdpmenezes@gmail.com>
2023-06-04 22:48:40 -0300

    Adicionar arquivos de projeto.

---
+++ CrudAngular/Controllers/HeroController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudAngular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<List<Hero>>> GetHero()
+        {
+            return new List<Hero>
+            {
+                new Hero
+                {
+                    HeroName = "Spider Man",
+                    FirstName = "Peter",
+                    Lastname = "Parker",
+                    Place = "New York",
+                }
+            };
+        }
+    }
+}
---
+++ CrudAngular/Hero.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudAngular
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string HeroName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+
+
+    }
+}
